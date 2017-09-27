338628fc0e527c190d482e7a3ad4d3f8274ba8df
efg95 <ecclesiastesgan@gmail.com>
2017-09-26 22:30:29 -0400

    Update Blackjack.java

--- Blackjack.java
+++ Blackjack.java
@@ -2,7 +2,7 @@
  * UNI: efg2123
  */
 public class Blackjack {
-
+//main class that runs the game
 	public static void main(String[] args) {
 		Game blackjack = new Game();//creates new game
 		System.out.println("May the force be with you");
